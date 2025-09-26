4173c82c054285d3a3a90f10119473c5f9eadfbc
VladimirMelo <vladimirmelo30@gmail.com>
2025-09-26 19:07:44 -0300

    Type writer h1 intro

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,4 +31,16 @@ $(function () {
         }
         return false;
     });
+
+    //############## TYPE WRITER
+    function typeWriter(elemento) {
+        const textoArray = elemento.innerHTML.split('');
+        elemento.innerHTML = '';
+        for (let i = 0; i < textoArray.length; i++) {
+            setTimeout(() => elemento.innerHTML += textoArray[i], 75 * i);
+        }
+    }
+
+    const titulo = document.querySelector('.type-writer');
+    typeWriter(titulo);
 });
